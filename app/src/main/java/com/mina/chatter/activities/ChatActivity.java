0380e91141b538edf636404a0b2374c7ba5b6a92
mina0maher <86135594+mina0maher@users.noreply.github.com>
2022-06-05 10:21:46 +0200

    designing chat ui

---
+++ app/src/main/java/com/mina/chatter/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.mina.chatter.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mina.chatter.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
